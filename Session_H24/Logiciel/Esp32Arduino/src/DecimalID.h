9e4dabd4079e8626200349eebb48af74d0317fa1
Manzur21 <128533627+MamouneBen@users.noreply.github.com>
2024-04-22 10:05:34 -0400

    4.1; Mise à jour

--- Session_H24/Logiciel/Esp32Arduino/src/DecimalID.h
+++ Session_H24/Logiciel/Esp32Arduino/src/DecimalID.h
@@ -1,35 +1,48 @@
 #ifndef DECIMAL_ID_H
 #define DECIMAL_ID_H
 
+/**
+ * @file DecimalID.h
+ * @brief Fichier d'entête pour la définition des identifiants décimaux utilisés dans les messages CAN.
+ * 
+ * Ce fichier contient les définitions des identifiants décimaux pour les différents capteurs et leurs données
+ * correspondantes transmises via le bus CAN. Chaque identifiant est unique et est utilisé pour différencier
+ * les types de données envoyées ou reçues dans le réseau CAN. Modifier cet en-tête pour ajouter ou modifier
+ * les identifiants selon les besoins spécifiques du projet.
+ * 
+ * Les identifiants sont utilisés pour catégoriser les données envoyées, facilitant ainsi la gestion et le décodage
+ * des trames CAN par les dispositifs récepteurs. Il est crucial de maintenir une cohérence dans les identifiants
+ * utilisés pour éviter des conflits ou des erreurs de communication.
+ */
+
 // Définir l'ID décimal souhaité ici
 #define DECIMAL_ID 528
 
-#endif
+// Liste des capteurs et leur ID correspondant
+#define TEMP_SET_1_ID 528 // Temperature Set 1
+#define TEMP_SET_2_ID 529 // Temperature Set 2
+#define TEMP_SET_3_ID 530 // Temperature Set 3
+#define ANALOG_VOLTAGE_ID 531 // Analog input Voltages
+#define DIGITAL_INPUT_STATUS_ID 532 // Digital input status
+#define MOTOR_POSITION_INFO_ID 533 // Motor position info
+#define CURRENT_INFO_ID 534 // Current Info
+#define VOLTAGE_INFO_ID 535 // Voltage info
+#define FLUX_IQ_INFO_ID 536 // Flux ID IQ info
+#define INTERNAL_VOLTAGES_ID 537 // Internal Voltages
+#define INTERNAL_STATES_ID 538 // Internal states
+#define FAULT_CODES_ID 539 // Fault Codes
+#define TORQUE_TIMER_INFO_ID 540 // Torque and Timer info
+#define MODULATION_FLUX_INFO_ID 541 // Modulation and Flux info
+#define FIRMWARE_INFO_ID 542 // Firmware info
+#define DIAG_DATA_ID 543 // Diag Data
+#define FAST_INFO_ID 544 // Fast info
+#define U2C_COMMAND_TXD_ID 545 // U2C Command Txd
+#define U2C_COMMAND_RXD_ID 546 // U2C Command Rxd
+#define COMMAND_MESSAGE_ID 547 // Command Message
+#define READ_WRITE_PARAM_CMD_ID 548 // Read Write Param Command
+#define READ_WRITE_PARAM_RSP_ID 549 // Read Write Param Response
+#define U2C_MESSAGE_RXD_ID 550 // U2C_Message_Rxd
+#define U2C_COMMAND_TXD2_ID 551 // U2C_Command_Txd
+#define BMS_CURRENT_LIMIT_ID 552 // BMS Current Limit
 
-/*          Liste des capteurs et leur ID correspondant:
-
-            decimalID = 528; //Temperature Set 1
-            decimalID = 529; //Temperature Set 2
-            decimalID = 530; //Temperature Set 3
-            decimalID = 531; //Analog input Voltages
-            decimalID = 532; //Digital input status
-            decimalID = 533; //Motor position info
-            decimalID = 534; //Current Info
-            decimalID = 535;  //Voltage info
-            decimalID = 536; //Flux ID IQ info
-            decimalID = 537;  //Internal Voltages
-            decimalID = 538;  //Internal states
-            decimalID = 539;  //Fault Codes
-            decimalID = 540;  //Torque and Timer info
-            decimalID = 541; //Modulation and Flux info
-            decimalID = 542;  //Firmware info
-            decimalID = 543; //Diag Data
-            decimalID = 544; //Fast info
-            decimalID = 545; //U2C Command Txd
-            decimalID = 546; //U2C Command Rxd
-            decimalID = 547; //Command Message
-            decimalID = 548; //Read Write Param Command
-            decimalID = 549; //Read Write Param Response
-            decimalID = 550;  //U2C_Message_Rxd
-            decimalID = 551;  //U2C_Command_Txd
-            decimalID = 552; //BMS Current Limit*/
+#endif // DECIMAL_ID_H
--- Session_H24/Logiciel/Esp32Arduino/src/main.cpp
+++ Session_H24/Logiciel/Esp32Arduino/src/main.cpp
@@ -6,7 +6,7 @@
 
 //  Date: 22 Avril 2024
 
-//  Matériel: Esp32-C3 devkit-02, MCP3221, TJA1050, Potentiometre
+//  Matériel: Esp32-C3 XIAO SEED, MCP3221, TJA1050, Potentiometre
 
 
 // Inclusion des bibliothèques nécessaires
@@ -29,34 +29,45 @@ const int printInfosSerie = 1;     // Contrôle l'affichage des informations sur
 // Prototypes de fonction
 double lireMCP3221(int adresseMCP3221, int printInfo);
 double convertDbyte_Double(byte msb, byte lsb);
-int printINFO(byte msb, byte lsb);
-void sendPotentiometerValue(double volts);
+int affichage(byte msb, byte lsb);
+void trame_CAN(double volts);
 int getDecimalID(int hexID);
 
 // Configuration initiale
 void setup() {
-    pinMode(GPIO_NUM_2, OUTPUT);
+    pinMode(GPIO_NUM_2, OUTPUT);    //Met le GPIO en sortie
     digitalWrite(GPIO_NUM_2, HIGH); // Mettre la broche GPIO en état haut
 
-    Wire.begin(GPIO_NUM_6, GPIO_NUM_7); // Initialiser le bus I2C
+    Wire.begin(GPIO_NUM_6, GPIO_NUM_7); // initialise le bus I2C
     Serial.begin(115200); // Démarrer la communication série à 115200 bauds
     dernierTemps = millis(); // Enregistrer le temps actuel
 
     // Configuration du bus CAN
-    ESP32Can.setPins(CAN_TX, CAN_RX); // Définir les broches pour le CAN
-    ESP32Can.setRxQueueSize(1); // Taille de la file d'attente de réception
-    ESP32Can.setTxQueueSize(1); // Taille de la file d'attente de transmission
-    ESP32Can.setSpeed(ESP32Can.convertSpeed(1000)); // Vitesse du bus CAN à 1000 kbps
+    ESP32Can.setPins(CAN_TX, CAN_RX); // Définit les broches pour le CAN GPIO 21/GPIO 20
+    ESP32Can.setRxQueueSize(1);  // Taille de la file d'attente de réception
+    ESP32Can.setTxQueueSize(1);  // Taille de la file d'attente de transmission
+    ESP32Can.setSpeed(ESP32Can.convertSpeed(1000)); // Vitesse du bus CAN à 1000 kbps 
 
     // Démarrage du bus CAN
     if (ESP32Can.begin()) {
-        Serial.println("Bus CAN démarré !");
+        Serial.println("Bus CAN démarré !"); 
     } else {
         Serial.println("Échec du démarrage du bus CAN !");
     }
 }
 
-// Lecture des données depuis le MCP3221
+/**
+ * @brief Lit les données analogiques depuis le MCP3221 via I2C et convertit ces données en valeur de tension.
+ * 
+ * Cette fonction initie une communication I2C avec le MCP3221 à l'adresse spécifiée, lit les octets de données (MSB et LSB),
+ * les convertit en tension
+ * 
+ * @param adresseMCP3221 L'adresse I2C du MCP3221 
+ * @param printInfo Contrôle l'affichage des informations de diagnostic sur le port série.
+ * 
+ * @return La valeur de tension en volts calculée à partir des données brutes lues.
+ */
+
 double lireMCP3221(int adresseMCP3221, int printInfo) {
     Wire.beginTransmission(adresseMCP3221);
     Wire.endTransmission();
@@ -65,19 +76,46 @@ double lireMCP3221(int adresseMCP3221, int printInfo) {
     byte octetMSB = Wire.read(); // Lire l'octet de poids fort
     byte octetLSB = Wire.read(); // Lire l'octet de poids faible
     if (printInfo) {
-        printINFO(octetMSB, octetLSB);
+        affichage(octetMSB, octetLSB); // appel fonction pour l'affichage
     }
-    return convertDbyte_Double(octetMSB, octetLSB);
+    return convertDbyte_Double(octetMSB, octetLSB); // retourne tension en volts
 }
 
-// Convertir les octets lus en une valeur de tension double
+
+
+/**
+ * @brief Convertit les octets de données en valeur de tension double.
+ * 
+ * Prend les octets MSB et LSB lus depuis le MCP3221, les combine pour former une valeur sur 12 bits,
+ * puis convertit cette valeur en tension réelle en utilisant la référence de tension et la résolution de l'ADC.
+ * 
+ * @param msb Octet de poids fort reçu du MCP3221.
+ * @param lsb Octet de poids faible reçu du MCP3221.
+ * 
+ * @return La valeur de tension en volts.
+ */
+
 double convertDbyte_Double(byte msb, byte lsb) {
-    int value = ((msb & 0x0F) << 8) | lsb; // Combinaison des deux octets en une valeur sur 12 bits
-    return (double)value * vRef / 4095.0; // Convertir en tension en utilisant la référence et la résolution
+    int valeurADC_12bits = ((msb & 0x0F) << 8) | lsb;   //  Déclaration variable valeur12bits qui va contenir la valeur numérique calculée sur 12 bits.
+                                                    //  Extrait les 4 bits bas du MSB, les décale de 8 bits à gauche, puis combine avec le LSB pour donner la valeurADC sur 12 bits
+
+    return (double)valeurADC_12bits * vRef / 4095.0;    //  Convertit la valeur sur 12 bits en tension réelle en utilisant la référence de tension de l'ADC (5v), puis retourne cette valeur.
+                                                    //  Volt = (AN * 5) / 4095
 }
 
-// Afficher les informations de diagnostic sur la valeur ADC et la tension calculée
-int printINFO(byte msb, byte lsb) {
+/**
+ * @brief Affiche les informations de diagnostic concernant la valeur ADC et la tension calculée.
+ * 
+ * Cette fonction calcule la valeur ADC à partir des octets MSB et LSB, convertit cette valeur en tension,
+ * puis imprime les valeurs ADC et la tension en millivolts sur le port série pour le diagnostic.
+ * 
+ * @param msb Octet de poids fort indiquant la partie haute de la valeur ADC.
+ * @param lsb Octet de poids faible indiquant la partie basse de la valeur ADC.
+ * 
+ * @return La valeur ADC sur 12 bits calculée à partir des octets MSB et LSB.
+ */
+
+int affichage(byte msb, byte lsb) {
     int valeurADC = ((msb & 0x0F) << 8) | lsb; // Calculer la valeur ADC
     Serial.print("Valeur ADC (12 bits) = ");
     Serial.print(valeurADC);
@@ -88,39 +126,56 @@ int printINFO(byte msb, byte lsb) {
     return valeurADC;
 }
 
-// Envoyer la valeur de tension mesurée sur le bus CAN
-void sendPotentiometerValue(double volts) {
+/**
+ * @brief Envoie la valeur de tension mesurée sur le bus CAN.
+ * 
+ * Cette fonction prépare une trame CAN en convertissant la valeur de tension en volts en deux composants:
+ * les volts entiers et les centièmes de volt. Elle place ces valeurs dans les deux premiers octets d'une trame CAN,
+ * met les autres octets à zéro pour le padding, et envoie cette trame sur le bus CAN.
+ * 
+ * @param volts La tension mesurée en volts à envoyer sur le bus CAN.
+ * 
+ * @return void Cette fonction ne retourne rien.
+ */
+
+void trame_CAN(double volts) {
     CanFrame potFrame = { 0 };
-    potFrame.identifier = DECIMAL_ID;
-    potFrame.extd = 0;
-    potFrame.data_length_code = 8;
+    potFrame.identifier = DECIMAL_ID; // Utilise l'identifiant décimal défini pour la trame CAN
+    potFrame.extd = 0; // Indique que c'est une trame standard (non étendue)
+    potFrame.data_length_code = 8; // Taille de données fixée à 8 octets
     
-    unsigned char canVolts = (int)volts;
-    unsigned char canCentiVolts = (int)((volts - canVolts) * 100);
+    // Convertir la tension en volts entiers et en centièmes de volts
+    unsigned char canVolts = (int)volts; // Partie entière des volts
+    unsigned char canCentiVolts = (int)((volts - canVolts) * 100); // Calcul des centièmes de volts
 
-    potFrame.data[0] = canVolts;
-    potFrame.data[1] = canCentiVolts;
+    potFrame.data[0] = canVolts; // Stocker les volts entiers dans le premier octet
+    potFrame.data[1] = canCentiVolts; // Stocker les centièmes de volts dans le deuxième octet
 
-    // Mettre à zéro les autres octets
+    // Initialiser les octets restants à zéro
     for (int i = 2; i < 8; i++) {
-        potFrame.data[i] = 0;
+        potFrame.data[i] = 0; // Mettre à zéro les autres octets
     }
 
+    // Afficher les informations sur la trame CAN à envoyer
     Serial.print("Envoi de trame CAN avec ID: ");
     Serial.println(potFrame.identifier);
     Serial.print("Données: ");
     for (int i = 0; i < 8; i++) {
-        Serial.print((int)potFrame.data[i]);
+        Serial.print((int)potFrame.data[i]);  // Afficher chaque octet sous forme de nombre entier
         Serial.print(" ");
     }
     Serial.println();
 
+    // Envoyer la trame sur le bus CAN
     ESP32Can.writeFrame(potFrame);
 }
 
+
 // Boucle principale du programme
 void loop() {
-    double valeurCanal = lireMCP3221(adresseCAN, printInfosSerie); // Lire la valeur du canal ADC
-    sendPotentiometerValue(valeurCanal); // Envoyer cette valeur via CAN
+    double LectureMCP3221 = lireMCP3221(adresseCAN, printInfosSerie);   // stocke La valeur de tension en volts dans la vairable LectureMCP,
+                                                                        // prend en oarametre l'adresse I2C du MCP3221
+
+    trame_CAN(LectureMCP3221); // Envoyer cette valeur via CAN
     delay(500); // Attendre un demi-seconde
 }
